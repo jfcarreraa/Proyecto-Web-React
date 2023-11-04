a0ecba4643fd627e98cb8bbba1e8d0755efc3e0c
alejandro12321 <jackassduarte@gmail.com>
2023-11-03 21:35:31 -0600

    posts actualizado

--- web1/src/components/Home.jsx
+++ web1/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Post from "./Post";
+import "../styles/styles.scss";
 
 const Home = () => {
   const [allPosts, setAllPosts] = useState([]);
@@ -8,6 +9,7 @@ const Home = () => {
   const [postsToShow, setPostsToShow] = useState(5);
   // eslint-disable-next-line
   const [postsToLoad, setPostsToLoad] = useState(5);
+  const [selectedPost, setSelectedPost] = useState(null);
 
   useEffect(() => {
     fetch("https://dummyjson.com/posts")
@@ -30,6 +32,14 @@ const Home = () => {
     setPostsToShow(postsToShow + postsToLoad);
   };
 
+  const handlePostClick = (post) => {
+    setSelectedPost(post);
+  };
+
+  const closePostModal = () => {
+    setSelectedPost(null);
+  };
+
   return (
     <div>
       <h1>{showAllPosts ? "All Posts" : "Your Posts"}</h1>
@@ -38,13 +48,41 @@ const Home = () => {
         {showAllPosts
           ? allPosts
               .slice(0, postsToShow)
-              .map((post) => <Post key={post.id} post={post} user={user} />)
+              .map((post) => (
+                <Post
+                  key={post.id}
+                  post={post}
+                  user={user}
+                  onPostClick={handlePostClick}
+                />
+              ))
           : filteredPosts.map((post) => (
-              <Post key={post.id} post={post} user={user} />
+              <Post
+                key={post.id}
+                post={post}
+                user={user}
+                onPostClick={handlePostClick}
+              />
             ))}
       </ul>
       {showAllPosts && postsToShow < allPosts.length && (
         <button onClick={showMorePosts}>Load More</button>
+      )}
+      {selectedPost && (
+        <div className="post-modal">
+          <div className="modal-content">
+            <h2>{selectedPost.title}</h2>
+            <p>{selectedPost.body}</p>
+            <p>Published by: {user.firstName}</p>
+            <ul>
+              {selectedPost.tags && (
+                <li>Tags: {selectedPost.tags.join(", ")}</li>
+              )}
+              <li>Reactions: {selectedPost.reactions}</li>
+            </ul>
+            <button onClick={closePostModal}>Close</button>
+          </div>
+        </div>
       )}
     </div>
   );
